b31aa34a13fee2953473d607fc652989e06bc058
Malin Olsson <miragia.solinde@gmail.com>
2021-12-21 10:15:33 +0100

    Update script.js
    added some more objects to my array

--- script.js
+++ script.js
@@ -5,5 +5,22 @@ const characters = [
         age: 21,
         class: Sorcerer,
         description: alalalalaskdasdk,
-    }
+        //img: ,
+    },
+    {
+        name: Galindea,
+        race: Aquatic-elf,
+        age: 107,
+        class: Bard/Wizard,
+        description: alalalalv askdasdk fgfg feafdfaf,
+        //img: ,
+    },
+    {
+        name: Vamaris,
+        race: Drow,
+        age: 182,
+        class: fighter,
+        description: alalalalv askdasdk fgfg feafdfaf,
+        //img: ,
+    },
 ]
